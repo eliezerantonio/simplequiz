ac47a2b108e46cde6996b4d819a1a2a5cf3cdd5c
eliezerantonio <eliezerfernandoantonio@gmail.com>
2020-08-27 03:43:24 +0100

    add text e botoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  final perguntaSelecionada=0;
+  final List<String> perguntas = [
+    'Qual é a sua cor favorita?',
+    'Qual é o seu animal favorito?'
+    'Qual tecnologia voce gosta?'
+  ];
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello Flutter!!!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz"),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            RaisedButton(
+              child: Text("Resposta 1"),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text("Resposta 2"),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text("Resposta 3"),
+              onPressed: responder,
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget responder(){
+    perguntaSelecionada++;
+    print("Resposta $perguntaSelecionada");
   }
 }
