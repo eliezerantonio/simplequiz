c794daeb5a34ab65e28a805f24017238ec48d8aa
eliezerantonio <eliezerfernandoantonio@gmail.com>
2020-08-27 03:06:26 +0100

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Hello Flutter!!!"),
+    );
+  }
+}
